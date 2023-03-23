1af78790a3dc8266f7f1791b1b0b255735376d16
swkovma <124539620+swkovma@users.noreply.github.com>
2023-03-23 23:31:14 +0600

    Update urls.py

--- vercel_app/urls.py
+++ vercel_app/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from .settings import STATIC_URL, STATIC_ROOT
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('example.urls')),
 ]
+
+urlpatterns.extend(static(STATIC_URL, document_root=STATIC_ROOT))
